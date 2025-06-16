aa37f0ddf25c9b284162faed2f669b8da877914c
Abhijeet <abhi22jeetu@gmail.com>
2025-06-16 22:48:51 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-dotenv.config('./env')
+dotenv.config({
+    path:'./env'
+})
+
+const port = process.env.PORT || 8080
 
 connectDB()
+.then(() => {
+    app.listen(port, ()=>{`Swrver is running at port : ${port} `})
+})
+.catch((error)=>{
+    console.log("MONGO db connection failed !!!!" , error)
+})
